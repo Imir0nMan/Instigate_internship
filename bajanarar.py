50114f05dc0a8914f2df24f61a721e5434dd2692
Miqayel <sornamen315@gmail.com>
2022-06-28 13:19:28 +0400

    Python testing version

--- bajanarar.py
+++ bajanarar.py
@@ -1,12 +1,24 @@
-#amenamec bajanarar python
+#amenamec bajanairar python
 def bajanarar(a,b):   
     if (b==0):
         return a
     else:
         return bajanarar(b,a%b)
 
-a = int(input("greq arajin tivy "))
-b = int(input("greq erkrord tivy "))
+with open('exit3.txt','w') as q:
+ with open('test3.txt', 'r') as pt:
+   #crrlne = [line.split() for line in pt]
+   x = pt.readlines()
+   for i in x:
+       k = eval(i.strip())
+       v,b = k
+       h = bajanarar(v,b)
+       q.write(str(h))
+       q.write('\n')
+       #print(h)
 
-num = bajanarar(a,b)
-print(num)
+#a = int(input("greq arajin tivy "))
+#b = int(input("greq erkrord tivy "))
+
+#num = bajanarar(a,b)
+#print(num)
