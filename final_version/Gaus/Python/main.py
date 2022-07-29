4568d13bc4aea53248aa024f708cbabfcd1773d2
Hovhannes <sornamen315@gmail.com>
2022-07-29 07:36:18 +0400

    fixed some bugs

--- final_version/Gaus/Python/main.py
+++ final_version/Gaus/Python/main.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(sys.path[0]),"py_class"))
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]),"../py_class"))
 from matrix import Matrix
 
 with open ('exit.txt', 'w') as out:
